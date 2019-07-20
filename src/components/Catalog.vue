f2f854b7e9ff4fb328dacdd757f10edaeb3ee604
millienanagara <mee233411@gmail.com>
2019-07-20 17:14:40 +0700

    Fix merge conflict

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -2,11 +2,7 @@
   <div id="catalog" class="container">
     <h2>KOI</h2>
     <div class="row">
-<<<<<<< HEAD
-      <div v-for="tea in teaKoi" v-bind:key="tea.id" class="col-12 col-sm-4">
-=======
       <div v-for="tea in teaKoi" :key="tea.teaId" class="col-12 col-sm-4">
->>>>>>> 727cfda... no errors
         <router-link :to="tea.teaRoute">
           <div class="card m-4">
             <img class="img-height" :src="tea.teaImage" />
@@ -38,11 +34,7 @@ export default {
       teaBrand: ["KOI", "KAMU"],
       teaKoi: [
         {
-<<<<<<< HEAD
-          id: 1,
-=======
           teaId: 100,
->>>>>>> 727cfda... no errors
           teaType: "Macchiato",
           teaImage: "/images/blackmac.jpg",
           teaRoute: "/orderconfirm/teakoimacchiato"
@@ -54,11 +46,7 @@ export default {
           teaRoute: "/orderconfirm/teakoibubble"
         },
         {
-<<<<<<< HEAD
-          id: 2,
-=======
           teaId: 102,
->>>>>>> 727cfda... no errors
           teaType: "Latte",
           teaImage: "/images/blacklatte.jpg",
           teaRoute: "/orderconfirm/teakoilatte"
@@ -66,31 +54,19 @@ export default {
       ],
       teaKamu: [
         {
-<<<<<<< HEAD
-          id: 3,
-=======
           teaId: 103,
->>>>>>> 727cfda... no errors
           teaType: "Macchiato",
           teaImage: "/images/blackmac.jpg",
           teaRoute: "/orderconfirm"
         },
         {
-<<<<<<< HEAD
-          id: 4,
-=======
           teaId: 104,
->>>>>>> 727cfda... no errors
           teaType: "Bubble Tea",
           teaImage: "/images/blacktea.jpg",
           teaRoute: "/orderconfirm"
         },
         {
-<<<<<<< HEAD
-          id: 5,
-=======
           teaId: 105,
->>>>>>> 727cfda... no errors
           teaType: "Latte",
           teaImage: "/images/blacklatte.jpg",
           teaRoute: "/orderconfirm"
